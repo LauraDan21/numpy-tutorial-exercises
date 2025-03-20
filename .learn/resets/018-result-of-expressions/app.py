ebb865d227863904a7f6a738f54e5ee7888ce47c
LauraDan21 <161901568+LauraDan21@users.noreply.github.com>
2025-03-20 17:14:54 +0000

    Numpy exercises tutorial

---
+++ .learn/resets/018-result-of-expressions/app.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+# vector_null = np.zeros(10)
+# print(vector_null)
+
+# amount_of_memory = vector_null.size * vector_null.itemsize
+# print(amount_of_memory)
+
+# np.info(np.add(2,3))
+
+# vector_null[4] = 1
+# print(vector_null)
+
+# vector = np.arange(10,50)
+# print(vector)
+
+# vector = np.array([0,1,2,3,4,5,6,7,8,9])
+
+# inverted_vector = vector[::-1]
+# print(inverted_vector)
+
+# vector = np.arange(9).reshape((3,3))
+# print(vector)
+
+# arr = np.array([1,2,0,0,4,0])
+# indexes = np.nonzero(arr)
+# print(indexes)
+
+# arr = np.eye(3)
+# print(arr)
+
+# arr = np.random.random(3)
+# print(arr)
+
+# arr = np.random.random(10)
+# print(np.max(arr))
+
+# arr = np.random.random(10)
+# print(np.mean(arr))
+
+# arr = np.ones((5,5))
+# arr[1:-1, 1:-1] = 0
+# print(arr)
+
+# arr = np.ones((3,3))
+# arr_relleno = np.pad(arr, pad_width=1, mode="constant")
+# print(arr_relleno)
+
+
+
